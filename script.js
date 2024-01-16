011b5988635bc4e4ad14b3201146a90a3258eb1c
BrunoFrantech <mr.brunosecundario@gmail.com>
2024-01-16 11:15:14 -0400

    OK

--- script.js
+++ script.js
@@ -13,20 +13,51 @@ function botaoAdicionar() {
         var novoItem = document.createElement("li")
         novoItem.textContent = novaTarefa;
 
+        //Criar botao remover quando adicionar nova tarefa
+        var botaoRemover = document.createElement("button");
+        botaoRemover.innerHTML = '<span class="material-symbols-outlined remover-icon">delete</span>';
+        botaoRemover.onclick = function() {
+            removerTarefa(this);
+        }
+        botaoRemover.style.display ='inline'
+
+        //Criando nova tarefa
         listaTarefas.appendChild(novoItem);
+        novoItem.appendChild(botaoRemover)
 
         //Limpar quando adicionar
         inputTarefa.value = "";
 
+        
+        //adicionando classe "clicavel" ao novoItem 
+        novoItem.classList.add('clicavel');
+        novoItem.addEventListener('click', function() {
+        this.classList.toggle('clicada')
+           });
+
+
     } else {
         alert('Digite uma tarefa para adicionar!')
-    }    
-
+    } 
+    
+  
 }
 
-//Adicionando ouvinte de evento ao pressionar a tecla 'ENTER'
+
+    //FUNÇÃO REMOVER TAREFA
+    function removerTarefa(botaoRemover) {
+        var novoItem = botaoRemover.parentElement;
+        novoItem.remove();
+    }
+
+    //Adicionando ouvinte de evento ao pressionar a tecla 'ENTER'
     document.querySelector('input#adicionarTarefa').addEventListener('keyup', function(enter) {
         if(enter.keyCode === 13) {
             botaoAdicionar();
         }
     })
+
+
+
+
+
